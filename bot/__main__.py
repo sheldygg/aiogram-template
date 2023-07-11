f90076d55420447c84dfe8bf3ecb877f98039793
sheldy <chikibamtioma@gmail.com>
2023-07-11 19:10:23 +0300

    config as local variable in main func

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,17 +2,17 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 
-from bot.config_reader import config
+from bot.config_reader import Settings
 from bot.handlers import setup_routers
 
 
 async def main() -> None:
+    config = Settings()
     bot = Bot(config.bot_token.get_secret_value(), parse_mode="HTML")
     dp = Dispatcher()
 
     dp.include_router(setup_routers())
     await dp.start_polling(bot)
-
 
 
 asyncio.run(main())
